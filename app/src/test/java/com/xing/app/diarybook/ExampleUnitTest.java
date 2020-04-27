029c5daad9f5b4877f10ef93318c2d034226ba34
Xing <366549434@qq.com>
2020-04-27 18:05:13 +0800

    多了个链表学习的代码

--- app/src/test/java/com/xing/app/diarybook/ExampleUnitTest.java
+++ app/src/test/java/com/xing/app/diarybook/ExampleUnitTest.java
@@ -12,6 +12,33 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+        int[] arr = new int[]{1,2,3};
+
+//        Op opPre = new Op();
+        Op op = new Op();
+        for (int i:arr) {
+            if (op.next == null) op.next = new Op();
+            op.next.value = i;
+            System.out.println("来个结果吧11->"+op.next.value);
+            op.next.pre = op;
+            op = op.next;
+        }
+
+        while (op.pre!=null) {
+            op = op.pre;
+            System.out.println("是否走到->"+op.value);
+        }
+
+        op.next.pre = null;
+
+        System.out.println("来个结果吧->"+op.next.next.next.value);
+
+    }
+
+    public class Op{
+        Op pre;
+        Op next;
+        int value;
     }
 }
